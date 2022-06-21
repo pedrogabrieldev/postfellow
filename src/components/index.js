4f708b449fda10b1e5ac2f176e491867e3293803
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-06-21 10:53:27 -0300

    Index new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import { Header } from './header'
 import { MainContainer } from './main-container'
+import { FormContainer } from './form-container'
+import { NewPostForm } from './new-post-form'
+import { TextInput } from './text-input'
 
-export { Header, MainContainer }
+export { Header, MainContainer, NewPostForm, FormContainer, TextInput }
