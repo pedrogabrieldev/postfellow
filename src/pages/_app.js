166883bdaf174a1c149a9044dda3ca6d03b62d02
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-06-20 22:57:04 -0300

    Create ThemeProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../styles/theme'
 import GlobalStyles from '../styles/global'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
