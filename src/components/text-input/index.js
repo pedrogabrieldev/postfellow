959f81a51d2a1c1b964a0457a01e31f2a879915c
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-06-21 10:52:31 -0300

    Create TextInput component

---
+++ src/components/text-input/index.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+const StyledTextInput = styled.input.attrs({
+  type: 'text',
+  placeholder: 'Digite seu nome',
+})`
+  width: 100%;
+  height: 40px;
+  padding: 12px 16px;
+  border: none;
+  border-radius: 8px;
+  background-color: #494949;
+  color: #ffffff;
+
+  &::placeholder {
+    color: #9f9f9f;
+  }
+`
+
+export const TextInput = () => {
+  return <StyledTextInput />
+}
