7c9b4ec9fe6ef1d131c2f28ed1e5b1c85164efa9
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-06-23 20:47:25 -0300

    Move FormContainer inside NewPostForm

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,19 +1,57 @@
-import {
-  FormContainer,
-  Avatar,
-  AvatarSelector,
-  TextInput,
-  TextArea,
-  Buttons,
-} from './'
+import styled from 'styled-components'
+import { Avatar, AvatarSelector, TextInput, TextArea, Buttons } from './'
 
+import { Trash } from 'phosphor-react'
 import Photo from '../../public/baby-yoda.png'
 
+const FormContainer = styled.form`
+  padding: 24px;
+  background-color: #313131;
+  border: 1px solid #3b3b3b;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 16px;
+  align-items: center;
+`
+
+const AvatarDiv = styled.div`
+  grid-column-start: 2;
+  grid-column-end: 3;
+  width: 88px;
+  height: 88px;
+`
+
+const TrashButton = styled.button`
+  width: 24px;
+  height: 24px;
+  border: none;
+  background: none;
+  cursor: pointer;
+`
+
 export const NewPostForm = () => {
-  const avatar = false
+  const avatar = true
   return (
     <FormContainer>
-      {avatar ? <Avatar avatar={Photo} /> : <AvatarSelector />}
+      {avatar ? (
+        <Container>
+          <AvatarDiv>
+            <Avatar avatar={Photo} />
+          </AvatarDiv>
+          <TrashButton>
+            <Trash size={24} color="#ff4600" weight="light" />
+          </TrashButton>
+        </Container>
+      ) : (
+        <AvatarSelector />
+      )}
       <TextInput />
       <TextArea />
       <Buttons />
