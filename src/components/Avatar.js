d61242250902b04f6707b4372d8e37ef4588efd9
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-06-24 16:29:46 -0300

    Use a default user image if avatar is not provided

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styled from 'styled-components'
+import DefaultUser from '../../public/default-user.png'
 
 const StyledDiv = styled.div`
   border-radius: 100%;
@@ -10,7 +11,7 @@ export const Avatar = (props) => {
   return (
     <StyledDiv>
       <Image
-        src={props.avatar}
+        src={props.avatar ? props.avatar : DefaultUser}
         width={1}
         height={1}
         layout="responsive"
