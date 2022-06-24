15f9310ac3e7215dec852a21f12d3300d8b369da
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-06-24 11:13:58 -0300

    Fix Header width

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const StyledHeader = styled.header`
   position: fixed;
   top: 0;
-  width: 100vw;
+  width: 100%;
   height: ${(props) => props.theme.sizes.headerHeight};
   background-color: #2b2b2b;
   color: ${(props) => props.theme.colors.green};
